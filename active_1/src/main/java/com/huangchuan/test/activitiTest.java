eb20f5f770393e907a6b6f0456e5a3d8cb51033e
hc-hello <18702522056@163.com>
2020-05-24 10:19:20 +0800

    add 1

---
+++ active_1/src/main/java/com/huangchuan/test/activitiTest.java
@@ -0,0 +1,13 @@
+package com.huangchuan.test;
+
+
+/**
+ * 测试类
+ *  作用：测试aciviti所需要的的25张表的生成
+ */
+public class activitiTest {
+
+    public void testGenTable(){
+        //1.创建ProcessEngineConfiguration对象
+    }
+}
